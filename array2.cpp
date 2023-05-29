d9c45819bda0ca78247d70fa18d3cbd68061493f
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-05-29 08:33:08 +0530

    Create array2.cpp

---
+++ array2.cpp
@@ -0,0 +1,15 @@
+//is sorted or not -codestudio
+int isSorted(int n, vector<int> a) {
+    // Write your code here.
+    for(int i=1;i<n;i++)
+     {
+         if(a[i]>=a[i-1])
+         {
+
+         }
+         else{
+             return false;
+         }
+     }
+     return true;
+}
